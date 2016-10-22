8183e2e0f7a3758b11393e8ea35ceb31d1379f71
A Tru <A Tru>
2016-10-22 04:37:20 -0500

    Added Name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,8 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
- $("#main").append(["Alex Truong"]); Minor change to see if this works.
+var name = "Alex Truong";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#header").append([formattedName]);
+ $("#header").append([formattedRole]);
