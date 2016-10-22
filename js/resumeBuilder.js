666a6ee613f11763e974bc96b531c5f7cb707be1
A Tru <A Tru>
2016-10-22 06:50:57 -0500

    a bunch of data entry

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,5 +4,86 @@ var formattedName = HTMLheaderName.replace("%data%", name);
 var role = "Web Developer";
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
- $("#header").append([formattedName]);
- $("#header").append([formattedRole]);
+ $("#header").prepend([formattedRole]);
+ $("#header").prepend([formattedName]);
+
+ var skills = 
+ ["awesomeness", "fun", "drinking"];
+
+ $("#main").append(skills);
+
+ var bio = {
+ 	"name" : "Alex Truong",
+ 	"role" : "Web Developer",
+ 	"Contacts" : {
+ 		"email" : "alexander.m.truong@gmail.com"
+ 	},
+ 	"WelcomeMessage" : "I am cool, thanks",
+ 	"skills" : [
+ 	"awesomeness", "fun", "eating"],
+ 	"biopic" : "images/animage.jpg"
+ }
+
+ var bio = {
+ 	"name" : "Alex Truong",
+ 	"role" :  "Web Developer",
+ 	"contacts" : {
+ 		"mobile" : "612-284-5194",
+ 		"email" : "alexander.m.truong@gmail.com",
+ 		"github" : "magic-onion",
+ 		"location" : "Saigon, Vietnam"
+ 	},
+ 	"welcomeMessage" : "Thanks for viewing my Resume",
+ 	"skills" : [
+ 		"HTML", "CSS", "JS", "Jquery"
+ 		],
+ 	"biopic" : "images/animage.jpg"
+ }
+
+ var education = {
+ 	"schools" : [
+ 	{
+ 		"name" : "University of Wisconsin - Madison",
+ 		"city" : "Madison, WI",
+ 		"degree" : "BS",
+ 		"majors" : "Economics, History",
+ 		"dates" : 2011,
+ 		"url" : "http://www.wisc.edu"
+ 	}
+ 	]
+ 	"onlineCourses" : [
+ 		{
+ 		"title" : "Front End Web Developer Nanodegree",
+ 		"school" : "Udacity",
+ 		"dates" : 2016,
+ 		"url" : "http://www.udacity.com",
+ 		}
+ 	]
+ }
+
+var work = {
+	"jobs" : [
+	{
+		"employer" : "405 Media Group",
+		"title" : "Project Manager",
+		"dates" : "May 2015 - Present",
+		"description" : "A BETTER DESCRIPTION HERE"
+	},
+	{
+		"employer" : "Printlab",
+		"title" : "Production Manager",
+		"dates" : "Aug 2014 - May 2015".
+		"description" : "A BETTER DESCRIPTION HERE"
+	}
+]
+}
+
+var projects = {
+	"projects" : [
+	{
+		"title" : "Sample Project 1",
+		"dates" : "2016",
+		"description" : "a description"
+	}
+	]
+}
